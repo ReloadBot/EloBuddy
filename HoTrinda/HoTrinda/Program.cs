30791223265e4d1dc5b691f5f14e6b7663939bde
ReloadBot <gaaahzl@hotmail.com>
2015-09-27 20:16:36 -0300

    Trinda

---
+++ HoTrinda/HoTrinda/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+using EloBuddy;
+using EloBuddy.SDK;
+using SharpDX;
+using EloBuddy.SDK.Events;
+using EloBuddy.SDK.Enumerations;
+using EloBuddy.SDK.Menu;
+using EloBuddy.SDK.Menu.Values;
+using EloBuddy.SDK.Rendering;
+using Color = System.Drawing.Color;
+
+namespace HoTrinda
+{
+
+
+    class Program
+    {
+
+        public static Menu Drawnigs, ComboMenu, menu, Healing;
+        public static Spell.Active Q;
+        public static Spell.Active W;
+        public static Spell.Active E;
+        public static Spell.Active R;
+    
+
+
+    static void Main(string[] args)
+        {
+            Loading.OnLoadingComplete += Loading_OnLoadingComplete;
+        }
+
+        private static void Loading_OnLoadingComplete(EventArgs arg)
+        {
+            
+            if (Player.Instance.ChampionName != "Tryndamere")
+            {
+                return;
+            }
+            
+
+
+        }
+        }
+}
+
