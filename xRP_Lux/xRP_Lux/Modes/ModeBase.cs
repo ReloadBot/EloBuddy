2c52e9947c2b5c20da2c40aab4b9b00fe2fedeb4
ReloadBot <gaaahzl@hotmail.com>
2015-10-13 17:45:06 -0300

    x

---
+++ xRP_Lux/xRP_Lux/Modes/ModeBase.cs
@@ -0,0 +1,30 @@
+﻿using EloBuddy.SDK;
+using xRp_Lux;
+
+namespace xRP_Lux.Modes
+{
+    public abstract class ModeBase
+    {
+
+        protected Spell.Skillshot Q
+        {
+            get { return SpellManager.Q; }
+        }
+        protected Spell.Active W
+        {
+            get { return SpellManager.W; }
+        }
+        protected Spell.Skillshot E
+        {
+            get { return SpellManager.E; }
+        }
+        protected Spell.Skillshot R
+        {
+            get { return SpellManager.R; }
+        }
+
+        public abstract bool ShouldBeExecuted();
+
+        public abstract void Execute();
+    }
+}
