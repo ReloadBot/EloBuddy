1c9130a53c03a64617845f3f9d9d26a91fc03d07
ReloadBot <gaaahzl@hotmail.com>
2015-10-19 12:30:35 -0200

    Future Gnar Addon

---
+++ xRP Gnar/xRP Gnar/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EloBuddy;
+using EloBuddy.SDK.Events;
+
+namespace xRP_Gnar
+{
+     class Program
+    {
+        public static AIHeroClient _player = ObjectManager.Player;
+
+        static void Main(string[] args)
+        {
+            Loading.OnLoadingComplete += OnLoaded;
+        }
+
+        private static void OnLoaded(EventArgs args)
+        {
+            if (_player.HasBuff("Mega Gnar"))
+            {
+                {
+                    return;
+                } 
+            }
+        }
+    }
+}
